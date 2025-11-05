3e4659966eca83eb44d93821ba824f6940ba5e74
Meunier Lisa <lisa.meunier71500@gmail.com>
2025-11-05 10:22:53 +0100

    FEAT: Ajout documentation classe Invite

--- Client/METIER/Invite.cs
+++ Client/METIER/Invite.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace METIER_Footies
 {
+    /// <summary>
+    /// Représentation d'un invité avec son identité et coordonées
+    /// </summary>
     public class Invite
     {
         #region Attributs
@@ -17,13 +20,35 @@ namespace METIER_Footies
         #endregion
 
         #region Propriétés
+        /// <summary>
+        /// Nom de famille de l'invité
+        /// </summary>
         public string Nom {  get => nom; set => nom = value; }
+
+        /// <summary>
+        // Prénom de l'invité
+        /// </summary>
         public string Prenom {  get => prenom; set => prenom = value; }
+
+        /// <summary>
+        // Téléphone de l'invité
+        /// </summary>
         public string Telephone { get => telephone; set => telephone = value; }
+
+        /// <summary>
+        // Email de l'invité
+        /// </summary>
         public string Email { get => email; set => email = value; }
         #endregion
 
         #region Constructeurs
+        /// <summary>
+        /// Constructeur d'un invité
+        /// </summary>
+        /// <param name="nom"> Nom de famille de l'invité </param>
+        /// <param name="prenom"> Prénom de l'invité </param>
+        /// <param name="telephone"> Téléphone de l'invité </param>
+        /// <param name="email"> Email de l'invité </param>
         public Invite(string nom, string prenom, string? telephone, string? email)
         {
             this.nom = nom;
