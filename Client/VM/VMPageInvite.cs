3292cd87c5401f521e82f7fa7fae264c9dc28c05
Meunier Lisa <lisa.meunier71500@gmail.com>
2025-11-05 12:47:52 +0100

    FEAT: Implémentation attributs dans VMPageInvite

--- Client/VM/VMPageInvite.cs
+++ Client/VM/VMPageInvite.cs
@@ -5,10 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using METIER_Footies;
+using METIER_Footies.Data;
+using METIER_Footies.Metier;
 
 namespace VM_Footies
 {
-    public class VMPageInvite
+    public class VMPageInvite : INotifyPropertyChanged
     {
+        #region Attributs
+        private List<VMInvite> listeInvites;
+        private InviteDAO inviteDAO;
+        #endregion
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public List<VMInvite> VMInvites => listeInvites;
+
+        #region Constructeurs
+        /*
+        public VMPageInvite()
+        {
+            this.inviteDAO = new InviteDAO();
+            this.listeInvites = new List<VMInvite>();
+
+            foreach (Invite invite in inviteDAO.ObtenirTout()) // coder la méthode ObtenirTout dans InviteDAO
+            {
+                this.listeInvites.Add(new VMInvite(invite));
+            }
+        }
+        */
+
+
+        #endregion
     }
 }
