be67fc52dfaef9e11d7ba83d921d4a99da1a8359
Meunier Lisa <lisa.meunier71500@gmail.com>
2025-11-05 12:23:18 +0100

    FEAT: Implémentation code VMInvite

--- Client/METIER/Data/InviteDAO.cs
+++ Client/METIER/Data/InviteDAO.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using METIER_Footies.Metier;
 
-namespace METIER_Footies
+namespace METIER_Footies.Data
 {
     /// <summary>
     // Classe d'accès aux données pour les invités avec la base de données
@@ -18,9 +19,9 @@ namespace METIER_Footies
         /// </summary>
         /// <param name="invite"> l'invité à ajouter </param>
         /// <returns> Réponse http de l'API </returns>
-        public async Task<HttpResponseMessage> AddInvite(Invite invite)
+        public async Task<HttpResponseMessage> AjouterInvite(Invite invite)
         {
-            HttpResponseMessage reponseHttp = await this.PostAsync("Invites/AjoutInvite", invite); 
+            HttpResponseMessage reponseHttp = await PostAsync("Invites/AjoutInvite", invite); 
             return reponseHttp;
         }
     }
