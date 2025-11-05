b27a906400d5224f4e4d254b9ba6eecad422ac98
Lambert-H <hugo.lambert@iut-dijon.u-bourgogne.fr>
2025-11-05 13:48:19 +0100

    Ajout de l'interface pour récupérer différentes informations via le DAO

---
+++ API/API_Footies/Data/Interfaces/ITypeDAO.cs
@@ -0,0 +1,15 @@
+﻿namespace API_Footies.Data.Interfaces
+{
+    /// <summary>
+    /// Définit les méthodes pour accéder et récupérer des informations dans une base de données
+    /// </summary>
+    public interface ITypeDAO
+    {
+        /// <summary>
+        /// Renvoie la liste de tous les invités
+        /// </summary>
+        /// <returns>La liste de tous les invités </returns>
+        Int64 GetIdTypeByNom(string nom);
+
+    }
+}
