5d97acc907f504118c9b940d84a2d142c61799d3
Lambert-H <hugo.lambert@iut-dijon.u-bourgogne.fr>
2025-11-05 13:58:06 +0100

    Ajoute de la classe InviteDAO
    Cette classe implémente l'interface IInviteDAO qui permet d'aller modifier la base de donnée

---
+++ API/API_Footies/Data/DAO/InviteDAO.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using API_Footies.Data.Interfaces;
+using API_Footies.Metier;
+
+namespace API_Footies.Data.DAO
+{
+    /// <summary>
+    /// Classe en charge de tout ce qui touche les invités dans la base de données
+    /// </summary>
+    public class InviteDAO : IInviteDAO
+    {
+
+        public bool AjouterInvite(Invite invite)
+        {
+            TypeDAO typeDAO = new TypeDAO();
+            bool ajoute = false;
+            using (SQLiteConnector connection = new SQLiteConnector())
+            {
+                if (connection == null)
+                {
+                    throw new Exception("Erreur de connexion à la base de données");
+                }
+                else
+                {
+                    var parameters = new Dictionary<string, object>()
+                    {
+
+                    {"@Id", typeDAO.GetIdTypeByNom(invite.Nom) },
+                    {"@Nom",invite.Nom },
+                    {"@Prenom",invite.Prenom },
+                    {"@Telephone",invite.Telephone },
+                    {"@Email",invite.Email }
+
+                    };
+                    invite.Id = connection.ExecuteInsert("INSERT INTO Invite (idInvite,nom,prenom,telephone,email) VALUES (@Id,@Nom,@Prenom,@Telephone, @Email)", parameters);
+                    ajoute = true;
+                }
+
+            }
+            return ajoute;
+        }
+    }
+}
