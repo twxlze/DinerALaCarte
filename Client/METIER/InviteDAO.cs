503a9f246e51acbd56e818d276fc730ddcc6f378
Meunier Lisa <lisa.meunier71500@gmail.com>
2025-11-05 10:41:57 +0100

    FEAT: Implémentation code InviteDAO

--- Client/METIER/InviteDAO.cs
+++ Client/METIER/InviteDAO.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace METIER_Footies
 {
-    public class InviteDAO
+    /// <summary>
+    // Classe d'accès aux données pour les invités avec la base de données
+    /// </summary>
+    public class InviteDAO : DAO
     {
+        /// <summary>
+        /// Ajoute un invité
+        /// </summary>
+        /// <param name="invite"> l'invité à ajouter </param>
+        /// <returns> Réponse http de l'API </returns>
+        public async Task<HttpResponseMessage> AddInvite(Invite invite)
+        {
+            HttpResponseMessage reponseHttp = await this.PostAsync("Invites/AjoutInvite", invite); 
+            return reponseHttp;
+        }
     }
 }
