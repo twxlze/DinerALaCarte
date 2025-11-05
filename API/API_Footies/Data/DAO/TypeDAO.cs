04c8377dd9c91ba31111024c2139efc10cdb552a
Lambert-H <hugo.lambert@iut-dijon.u-bourgogne.fr>
2025-11-05 13:56:01 +0100

    Ajout de classe TypedDAO
    Cette classe implémente l'interface ITypeDAO. TypeDAO demande à la base de donnée des informations

---
+++ API/API_Footies/Data/DAO/TypeDAO.cs
@@ -0,0 +1,29 @@
+﻿using API_Footies.Data.Interfaces;
+
+namespace API_Footies.Data.DAO
+{
+    /// <summary>
+    /// Récupère l'identifiant unique d'un type en fonction de son nom.
+    /// </summary>
+    public class TypeDAO : ITypeDAO
+    {
+        public long GetIdTypeByNom(string nom)
+        {
+            Int64 id = -1;
+            using (SQLiteConnector connection = new SQLiteConnector())
+            {
+                var parameters = new Dictionary<string, object>()
+                {
+                    {"@nom",nom }
+                };
+                var data = connection.ExecuteQuery("SELECT * FROM Invite WHERE nom=@nom", parameters);
+                if (data.Rows.Count > 0)
+                {
+                    id = data.Rows[0].Field<Int64>("idInvite");
+                }
+
+            }
+            return id;
+        }
+    }
+}
