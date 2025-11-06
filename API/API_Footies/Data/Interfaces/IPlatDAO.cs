0a7dda0cec8c51e9066c42693a92bd56900664c5
Bastien.mtk <bastien.matokwong@iut-dijon.u-bourgogne.fr>
2025-11-06 10:52:23 +0100

    Ajout des classes IPlatService.cs et IPlatDAO.cs

---
+++ API/API_Footies/Data/Interfaces/IPlatDAO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using API_Footies.Metier;
+
+namespace API_Footies.Data.Interfaces
+{
+    /// <summary>
+    /// Définit la gestion des opérations liées aux plats
+    /// </summary>
+    public interface IPlatDAO
+    {
+        /// <summary>
+        /// Ajouter un plat
+        /// </summary>
+        /// <param name="invite"> Plat à ajouter </param>
+        /// <returns> True si ajouté False sinon </returns>
+        bool AjouterPlat(Plat plat);
+    }
+}
---
+++ API/API_Footies/Services/Realisations/PlatService.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using API_Footies.Data.Interfaces;
+using API_Footies.Metier;
+using API_Footies.Services.Interfaces;
+
+namespace API_Footies.Services.Realisations
+{
+    /// <summary>
+    /// Fournit des services pour gérer les plats
+    /// </summary>
+    public class PlatService : IPlatService
+    {
+        #region attributes
+
+        private IPlatDAO dao;
+
+        #endregion
+
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe PlatService.
+        /// </summary>
+        /// <param name="dao">Injection de dépendance</param>
+        /// <param name="typeService">Service utilisé pour gérer les opérations liées aux types associées aux plats</param>
+        public PlatService(IPlatDAO dao)
+        {
+            this.dao = dao;
+        }
+
+        #region methods
+        /// <summary>
+        /// Ajoute un plat
+        /// </summary>
+        /// <param name="invite">plat à ajouté</param>
+        public void AjouterPlat(Plat plat)
+        {
+            this.dao.AjouterPlat(plat);
+        }
+        #endregion
+
+    }
+}
