bda32f06dc29cd4c71e0a240724c269af38208f3
Bastien.mtk <bastien.matokwong@iut-dijon.u-bourgogne.fr>
2025-11-06 08:37:00 +0100

    IHM VueInvites et début IHM MainWindow

---
+++ Client/IHM/VueInvites.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using IHM;
+
+namespace IHM_Footies
+{
+    /// <summary>
+    /// Logique d'interaction pour VueInvites.xaml
+    /// </summary>
+    public partial class VueInvites : Window
+    {
+        public VueInvites(MainWindow mainWindows)
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private async void BoutonVueAccueil(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            await Task.Delay(500);
+            this.Close();
+        }
+    }
+}
