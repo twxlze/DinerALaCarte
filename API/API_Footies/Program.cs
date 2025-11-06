2e96a8ac8288fa76b5ec9c7f0dc7ece73b499144
Mamadou-Amadou-LY <mamadou.ly@iut-dijon.u-bourgogne.fr>
2025-11-06 15:13:42 +0100

    gestion des coherences pour une base communes

--- API/API_Footies/Program.cs
+++ API/API_Footies/Program.cs
@@ -3,37 +3,40 @@ using API_Footies.Data.Interfaces;
 using API_Footies.Services.Interfaces;
 using API_Footies.Services.Realisations;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-/// ---- Injections des dépendances ----
-builder.Services.AddScoped<IInviteDAO, InviteDAO>();
-builder.Services.AddScoped<IInviteService, InviteService>();
-
-
-builder.Services.AddScoped<IInviteDAO, InviteDAO>();
-
-SQLitePCL.Batteries.Init();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        /// ---- Injections des dépendances ----
+        builder.Services.AddScoped<IInviteDAO, InviteDAO>();
+        builder.Services.AddScoped<IInviteService, InviteService>();
+
+        SQLitePCL.Batteries.Init();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
