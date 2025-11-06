aafcba069cd10602bebfa25cc9736b3d2838aeed
Bastien.mtk <bastien.matokwong@iut-dijon.u-bourgogne.fr>
2025-11-06 10:44:43 +0100

    Ajout de la classe IPlatService.cs

---
+++ API/API_Footies/Services/Interfaces/IPlatService.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using API_Footies.Metier;
+
+namespace API_Footies.Services.Interfaces
+{
+    public interface IPlatService
+    {
+        /// <summary>
+        /// Ajouter un plat dans la base de données
+        /// </summary>
+        /// <param name="plat"> Plat à ajouter </param>
+        /// <returns> Plat ajouter </returns>
+        void AjouterPlat(Plat plat);
+
+
+    }
+}
