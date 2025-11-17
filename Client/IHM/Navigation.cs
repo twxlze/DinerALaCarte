1f36a0edc4e360bb5b509dd611866826125545d8
Payre Timothée <payre.timothee@outlook.com>
2025-11-17 15:27:14 +0100

    API : Ajout du Métier Menu

--- Client/IHM/Navigation.cs
+++ Client/IHM/Navigation.cs
@@ -72,6 +72,7 @@ namespace IHM_Footies
         {
             VueFormulaireGroupeInvite vueFormulaireGroupeInvite = new VueFormulaireGroupeInvite();
             vueFormulaireGroupeInvite.Show();
+        }
         public static void AllerFormulaireMenu(Window fenetreActuelle)
         {
             VueFormulaireMenu vueFormulaireMenu = new VueFormulaireMenu();
