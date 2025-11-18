541c6e2111fd9a49f4bbb6db035c240df8139214
Mamadou-Amadou-LY <mamadou.ly@iut-dijon.u-bourgogne.fr>
2025-11-18 10:02:02 +0100

    Refactorisation de l'interface IGroupeInviteService pour une meilleur gestion des services de groupeInvite.

--- API/API_Footies/Services/Interfaces/IGoupeInvitesService.cs
+++ API/API_Footies/Services/Interfaces/IGoupeInvitesService.cs
@@ -8,40 +8,27 @@ namespace API_Footies.Services.Interfaces
     public interface IGroupeInvitesService
     {
         /// <summary>
-        /// Ajoute un groupe d'invités dans la base de données
+        /// créer un groupe d'invités dans la base de données
         /// </summary>
-        /// <param name="groupeInvites"> le groupe a ajouter representer par son nom </param>
-        GroupeInvites AjouterGroupeInvite(GroupeInvites groupeInvites);
-
-        /// <summary>
-        /// Ajouter un invité à un groupe d'invités
-        /// <param name="idGroupeInvites">l'id du groupe d'invités</param>
-        /// <param name="invite">l'invité à ajouter</param>
-        GroupeInvites AjouterInviteAuGroupe(long idGroupeInvites, Invite invite);
+        /// <param name="groupeInvites"> le groupe a ajouter</param>
+        void AjouterGroupeInvites(GroupeInvites groupeInvites);
 
         /// <summary>
         /// Récupère tous les groupes d'invités
         /// </summary>
         /// <returns> les groupes d'invite</returns>
-        List<GroupeInvites> RecupererTousGroupesInvites();
-
-        /// <summary>
-        /// Récupère les invités d'un groupe via son ID
-        /// </summary>
-        /// <param name="idGroupeInvites"></param>
-        /// <returns>le groupe avec son nom et ses invites</returns>
-        public GroupeInvites RecupereGroupeViaId(long idGroupeInvite);
+        List<GroupeInvites> ListeGroupesInvites();
 
         /// <summary>
         /// Modifie un groupe d'invités dans la base de données
         /// </summary>
         /// <param name="groupeInvite">le groupe d'invite</param>
         /// <returns>le groupe d'invite modifier</returns>
-        GroupeInvites ModifierGroupe(GroupeInvites groupeInvite);
+        void ModifierGroupeInvite(GroupeInvites groupeInvite);
 
         /// <summary>
         ///  Supprime un groupe d'invités via son ID
         ///</summary>
-        GroupeInvites SupprimerGroupe(long idGroupeInvite);
+        void SupprimerGroupe(long idGroupeInvite);
     }
 }
