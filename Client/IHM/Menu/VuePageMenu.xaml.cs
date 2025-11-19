c1eedb838d1c1fd2e822cb7e932ed398f6f43102
Bastien.mtk <bastien.matokwong@iut-dijon.u-bourgogne.fr>
2025-11-19 14:40:54 +0100

    Naviagtion des pages liées aux menus

--- Client/IHM/Menu/VuePageMenu.xaml.cs
+++ Client/IHM/Menu/VuePageMenu.xaml.cs
@@ -199,54 +199,83 @@ namespace IHM_Footies.Menu
 
         #region Boutons de navigation
         /// <summary>
-        /// Bouton pour aller à la vue d'accueil
+        /// Bouton pour aller à la page plat
         /// </summary>
-        /// <param name="sender"> L'expéditeur </param>
-        /// <param name="e"> Les arguments de l'événement </param>
-        private void BoutonAccueil_Click(object sender, RoutedEventArgs e)
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerPlat_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerPlat(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à l'accueil
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerAccueil_Click(object sender, RoutedEventArgs e)
         {
             Navigation.AllerAccueil(this);
         }
 
         /// <summary>
-        /// Bouton pour aller à la vue des invités
+        /// Bouton pour aller au menu
         /// </summary>
-        /// <param name="sender"> L'expéditeur </param>
-        /// <param name="e"> Les arguments de l'événement </param>
-        private void BoutonInvite_Click(object sender, RoutedEventArgs e)
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerMenu(this);
+        }
+        /// <summary>
+        /// Bouton pour aller à la page invité
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerInvite_Click(object sender, RoutedEventArgs e)
         {
             Navigation.AllerInvites(this);
         }
 
         /// <summary>
+        /// Bouton pour aller à la page des réglages
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerReglages_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerReglages(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page des groupes d'invités
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerGroupeInvite_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerGroupesInvites(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page des invitations
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerInvitation_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        /// <summary>
         /// Bouton pour fermer la fenêtre
         /// </summary>
-        /// <param name="sender"> L'expéditeur du clic </param>
-        /// <param name="e"> Les arguments de l'événement </param>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ButonFermerFenetre_Click(object sender, RoutedEventArgs e)
         {
             Navigation.FermerFenetre(this);
         }
 
-        /// <summary>
-        /// Bouton pour aller à la page menu
-        /// </summary>
-        /// <param name="sender"> L'expéditeur </param>
-        /// <param name="e"> les arguments de l'événement </param>
-        private void BoutonMenu_Click(object sender, RoutedEventArgs e)
-        {
-            // Navigation.AllerMenu(this);
-        }
-
-        /// <summary>
-        /// Bouton pour aller à la page plat
-        /// </summary>
-        /// <param name="sender"> L'expéditeur </param>
-        /// <param name="e"> les arguments de l'événement </param>
-        private void BoutonPlat_Click(object sender, RoutedEventArgs e)
-        {
-            Navigation.AllerPlat(this);
-        }
         #endregion
     }
 }
