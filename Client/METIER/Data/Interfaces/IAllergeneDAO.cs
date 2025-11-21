bf7a2893ff6e392bd78f615ca15918f86683fd25
Payre Timothée <payre.timothee@outlook.com>
2025-11-21 12:44:58 +0100

    Ajout de allergeneDAO et suppression d'ingredients dao

---
+++ Client/METIER/Data/Interfaces/IAllergeneDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using METIER_Footies.Metier;
+
+namespace METIER_Footies.Data.Interfaces
+{
+    /// <summary>
+    /// Interface pour les opérations des allergenes
+    /// </summary>
+    public interface IAllergeneDAO
+    {
+        List<Allergene> ListeAllergene();
+    }
+}
