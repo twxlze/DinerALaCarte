6f32a0b14975ffd7c8cca12abae5d230e1611918
Meunier Lisa <lisa.meunier71500@gmail.com>
2025-12-08 10:53:09 +0100

    Suppression allergènes /API

--- API/API_Footies/Program.cs
+++ API/API_Footies/Program.cs
@@ -30,10 +30,6 @@ builder.Services.AddScoped<IMenuService, MenuService>();
 builder.Services.AddScoped<IInvitationDAO, InvitationDAO>();
 builder.Services.AddScoped<IInvitationService, InvitationService>();
 
-builder.Services.AddScoped<IAllergeneDAO, AllergeneDAO>();
-builder.Services.AddScoped<IAllergeneService, AllergeneService>();
-
-
 SQLitePCL.Batteries.Init();
 
 WebApplication app = builder.Build();
