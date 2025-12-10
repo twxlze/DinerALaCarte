01a2f5be6655317f2647daf54e816c2f45785bfa
Mamadou-Amadou-LY <mamadou.ly@iut-dijon.u-bourgogne.fr>
2025-12-10 22:42:21 +0100

    Sprint3 - statistique : Separation.

--- Client/VM/VM-Page/VmPageStatistique.cs
+++ Client/VM/VM-Page/VmPageStatistique.cs
@@ -10,73 +10,10 @@ using VM_Footies.VM;
 using OxyPlot;
 using OxyPlot.Series;
 using OxyPlot.Axes;
+using VM_Footies.VM_Element_Selectionne;
 
 namespace VM_Footies.VM_Page
 {
-    /// <summary>
-    /// Modèle de vue pour les statistiques d'invite selectionner
-    /// </summary>
-    public class VMinviteStats : INotifyPropertyChanged
-    {
-        #region Attributs
-        private VMInvite _invite;
-        private bool _estSelectionne;
-        #endregion
-
-        #region Propriétés
-        public string Identite => _invite.Identite;
-
-        /// <summary>
-        /// Invite associée
-        /// </summary>
-        public VMInvite Invite
-        {
-            get { return _invite; }
-            set
-            {
-                _invite = value;
-                Notify("Invite");
-            }
-        }
-        /// <summary>
-        /// Indique si l'invité est sélectionné
-        /// </summary>
-        public bool EstSelectionne
-        {
-            get { return _estSelectionne; }
-            set
-            {
-                _estSelectionne = value;
-                Notify("EstSelectionne");
-            }
-        }
-
-        /// <summary>
-        /// Événement déclenché lorsqu'une propriété change
-        /// </summary>
-        public event PropertyChangedEventHandler? PropertyChanged;
-        #endregion
-
-        #region Constructeur
-        /// <summary>
-        /// Constructeur de VMinviteStats
-        /// </summary>
-        /// <param name="invite">prend un vminvite</param>
-        public VMinviteStats(VMInvite invite)
-        {
-            _invite = invite;
-            _estSelectionne = false;
-        }
-        #endregion
-
-        #region Méthodes protégées
-        protected void Notify(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
-    }
-
     /// <summary>
     /// Modèle de vue pour la page des statistiques
     /// </summary>
