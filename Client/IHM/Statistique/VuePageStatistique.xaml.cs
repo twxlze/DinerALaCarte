b1fb48e6dcbf08dd0d975a9b1addf74d840ef63a
Mamadou-Amadou-LY <mamadou.ly@iut-dijon.u-bourgogne.fr>
2025-12-10 12:30:15 +0100

    Feat Sprint3 - Statistique : Ajout de l'arborescence pour la gestion des statistique des invite, ajout entre autre de la classe vmPageStatistique, de la classe VMinviteStats, des fenetre de selection et de visionnement du graphique.
    installation des packages nuget pour oxyplot.

---
+++ Client/IHM/Statistique/VuePageStatistique.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IHM_Footies.Statistique
+{
+    /// <summary>
+    /// Logique d'interaction pour VuePageStatistique.xaml
+    /// </summary>
+    public partial class VuePageStatistique : Window
+    {
+        public VuePageStatistique()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Client/VM/VM-Page/VmPageStatistique.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VM_Footies.VM_Page
+{
+    /// <summary>
+    /// Modèle de vue pour les statistiques d'invite selectionner
+    /// </summary>
+    public class VMinviteStats : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
+    /// <summary>
+    /// Modèle de vue pour la page des statistiques
+    /// </summary>
+    public class VmPageStatistique : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
