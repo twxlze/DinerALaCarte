9d58bb04881623c719c7f5cfca0aa42fee98ee94
Mamadou-Amadou-LY <mamadou.ly@iut-dijon.u-bourgogne.fr>
2025-12-12 06:00:58 +0100

    SPRINT3 - Tableau de bord : - implementation de code behind de la fenetre VuePageTableauDeBord

---
+++ Client/IHM/TableauDeBord/VuePageTableauDeBord.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VM_Footies.VM;
+using VM_Footies.VM_Page;
+
+namespace IHM_Footies.TableauDeBord
+{
+    /// <summary>
+    /// Logique d'interaction pour VuePageTableauDeBord.xaml
+    /// </summary>
+    public partial class VuePageTableauDeBord : Window
+    {
+        #region Atributs    
+        private VMPageTableauDeBord vmPageTableauDeBord;
+        #endregion
+
+        #region Constructeur
+        public VuePageTableauDeBord()
+        {
+            InitializeComponent();
+            this.vmPageTableauDeBord = new VMPageTableauDeBord();
+            this.DataContext = this.vmPageTableauDeBord;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Initialiser();
+        }
+        #endregion
+
+        #region Methode public
+        public async void Initialiser()
+        {
+            await this.vmPageTableauDeBord.ChargerDonneesInvite();
+            this.listeInviteStat.Children.Clear();
+            foreach (VMInvite invite in this.vmPageTableauDeBord.ListeInvites)
+            {
+                VMStats vMStats = this.vmPageTableauDeBord.ChargerInvitationsParticipe(invite);
+                VueTableauDeBord vueTableauDeBord = new VueTableauDeBord(vMStats);
+                this.listeInviteStat.Children.Add(vueTableauDeBord);
+            }
+        }
+        #endregion
+
+
+        #region Boutons de navigation
+        /// <summary>
+        /// Bouton pour aller à la page plat
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerPlat_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerPlat(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à l'accueil
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerAccueil_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerAccueil(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller au menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerMenu(this);
+        }
+        /// <summary>
+        /// Bouton pour aller à la page invité
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerInvite_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerInvites(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page des réglages
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerReglages_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerReglages(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page des groupes d'invités
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerGroupeInvite_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerGroupesInvites(this);
+        }
+
+
+        /// <summary>
+        /// Bouton pour aller à la page des invitations
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerInvitation_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerInvitations(this);
+        }
+
+        /// Bouton pour fermer la fenêtre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButonFermerFenetre_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.FermerFenetre(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page des Statistiques
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerStatistique_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerSelectionInvite(this);
+        }
+
+        /// <summary>
+        /// Bouton pour aller à la page tableau de bord
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoutonAllerTableauDeBord_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.AllerTableaudebord(this);
+        }
+        #endregion
+
+    }
+}
