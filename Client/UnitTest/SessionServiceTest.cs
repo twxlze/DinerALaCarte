cc7861921013f545651147019a5d01b5a1aecb2f
Bastien.mtk <bastien.matokwong@iut-dijon.u-bourgogne.fr>
2025-12-12 14:56:29 +0100

    Ajout de la classe de tests unitaires pour session service

---
+++ Client/UnitTest/SessionServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using METIER_Footies.Metier;
+
+namespace Test_Footies_METIER
+{
+    public class SessionServiceTest
+    {
+        [Fact]
+        public void Instance_EstUniqueEtNonNulle()
+        {
+            SessionService instance1 = SessionService.Instance;
+            SessionService instance2 = SessionService.Instance;
+
+            Assert.NotNull(instance1);
+            Assert.Same(instance1, instance2);
+        }
+
+        [Fact]
+        public void GestionConnexion_FonctionneCorrectement()
+        {
+            SessionService session = SessionService.Instance;
+            session.UtilisateurConnecte = null;
+
+            Assert.False(session.EstConnecte, "Devrait être faux quand UtilisateurConnecte est null");
+
+            Utilisateur user = new Utilisateur(1, "Joueur1", "Nom", "Prenom", "06", "mail");
+            session.UtilisateurConnecte = user;
+
+            Assert.NotNull(session.UtilisateurConnecte);
+            Assert.True(session.EstConnecte, "Devrait être vrai une fois l'utilisateur défini");
+            Assert.Equal("Joueur1", session.UtilisateurConnecte.Pseudo);
+
+            session.UtilisateurConnecte = null;
+
+            Assert.False(session.EstConnecte, "Devrait redevenir faux après déconnexion");
+        }
+    }
+}
