9a80afc34cd13ae13274145a067a111bc7e023ec
Lambert-H <hugo.lambert@iut-dijon.u-bourgogne.fr>
2025-12-12 07:50:23 +0100

    Ajout de l'ajout d'un avis dans le controller dans l'API

--- API/API_Footies/Controllers/PlatController.cs
+++ API/API_Footies/Controllers/PlatController.cs
@@ -86,5 +86,28 @@ namespace API_Footies.Controllers
         {
             return this.service.ChercherPlat(texterecherche, idUtilisateur);
         }
+
+        /// <summary>
+        /// Ajoute ou modifie une note et un commentaire pour un plat
+        /// </summary>
+        /// <param name="avis">L'objet contenant les IDs, la note et le commentaire</param>
+        [HttpPost("AjouterAvis")]
+        public IActionResult AjouterAvis(Avis avis)
+        {
+            try
+            {
+                this.service.AjouterAvis(avis);
+                return Ok("Avis enregistré avec succès.");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message); // Erreur 400 si note invalide (ex: 12/10)
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erreur serveur : " + ex.Message);
+            }
+        }
+
     }
 }
